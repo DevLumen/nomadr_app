2115784aed30016ab0673740f744d2e048065c3a
Andres S <devdre783@Andress-MacBook-Pro.local>
2024-05-14 15:48:51 -0700

    fixed media queries laptop on van listings page

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,10 +6,10 @@ import './Footer.css'
 function Footer() {
     return (
         <nav className='footer'>
-            <p id='developed-by'>Developed By: Andres Manuel Soca</p>
-            <a style={{position: "absolute"}} target="_blank" className='github' href='https://github.com/DevDre783/Nomadr-AirBnB-Clone'><i style={{color: "white"}} className="fab fa-github"></i></a>
+            <p id='developed-by'>Developed By: Andres Soca</p>
+            <a style={{position: "absolute"}} target="_blank" className='github' href='https://github.com/DevLumen/nomadr_app'><i style={{color: "white"}} className="fab fa-github"></i></a>
             <a target="_blank" className='linkedIn' href='https://www.linkedin.com/in/andres-soca-23852aab/'><i style={{color: "white"}} className="fab fa-linkedin-in"></i></a>
-            <p id="my-email">Lumen.development01@gmail.com</p>
+            <p id="my-email">andres.soca@proton.me</p>
         </nav>
     )
 }
